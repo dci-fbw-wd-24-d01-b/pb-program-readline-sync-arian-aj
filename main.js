b1daff7aa269b873c425494aa3fa10c9658b3a64
Arian-Study <aristudy237@gmail.com>
2024-05-10 09:58:25 +0200

    fertig

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import readlineSync from "readline-sync";
+
+
+const myName = readlineSync.question('Wie Heißt du? ');
+const myBirthYear = readlineSync.question('In welchem Jahr bist du geboren? ');
+const myBirthCity = readlineSync.question('Wo bist du geboren? ');
+const currentCity = readlineSync.question('Wo lebst du jetzt? ');
+const currentYear = readlineSync.question('Was ist das aktuelle Jahr? ');
+
+let message = `Hallo, meine Name ist ${myName}, Ich wurde vor ${currentYear - myBirthYear} in ${myBirthCity} geboren. Jetzt lebe ich in ${currentCity}`;
+
+console.log(message)
